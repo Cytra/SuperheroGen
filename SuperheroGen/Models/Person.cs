8cf6368da361b24f04bc14d01940deaaf4d2256a
Cytra <samuilovas@gmail.com>
2019-02-07 18:35:02 +0200

    initial commit

---
+++ SuperheroGen/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroGen.Models
+{
+    /// <summary>
+    /// Person object
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// First name of the Person object
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName of the person object
+        /// </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        private string _heroName;
+        /// <summary>
+        /// SuperHero name
+        /// </summary>
+        
+        public string  HeroName
+        {
+            get { return _heroName; }
+        }
+
+        public void SetHeroName()
+        {
+            _heroName = "Batman";
+        }
+    }
+}
